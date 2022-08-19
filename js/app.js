cbbbafd6061f6e1474fd53863547c74cfd00eff6
ERIK M. DODD <emdodd27@gmail.com>
2022-08-19 14:31:28 -0700

    completed mutiplication question

--- js/app.js
+++ js/app.js
@@ -28,8 +28,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  let result = a * b;
-  return [result, 'The product of 5 and 9 is 45.'];
+    let result = a * b;
+    return [result, 'The product of 5 and 9 is 45.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
